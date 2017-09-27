f8df30849daa7b2fbc95f99bc9e1a3e51e4a965b
Emily Butte <emily.butte@gmail.com>
2017-09-27 11:29:32 -0400

    updates to contact form

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -18,3 +18,8 @@ $(document).ready(function () {
   }
   $(this).addClass('active')
 })
+
+$('#thumbnailImage').click(function () {
+   $(this).attr('width', '400')
+    $(this).attr('height', '300')
+})
